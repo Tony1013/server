b0f764d0e3f07c2255acf374a92ecb6be6eeff45
Anthony Tamayo <anthonyatamayo@gmail.com>
2015-06-30 18:23:36 -0400

    Update to Stock Ticker - add page to Heroku site

--- server.rb
+++ server.rb
@@ -13,3 +13,19 @@ end
 get '/hello' do
 	"Hello Bella and Mateo!"
 end
+
+	
+
+get '/stock_ticker' do
+	require 'HTTParty'
+	require 'nokogiri'
+
+	response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+
+	dom = Nokogiri::HTML(response.body)
+
+	list_of_results = dom.xpath("//span[@id='yfs_l84_aapl']")
+	result = list_of_results.first
+	
+	"The current price of APPL is $#{result.text}."
+end
