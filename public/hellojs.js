20bea90691370d7f67f1bb46710c8b2bdc18aa9d
Anthony Tamayo <anthonyatamayo@gmail.com>
2015-08-20 06:59:16 -0400

    commit changes to public file

---
+++ public/hellojs.js
@@ -0,0 +1,16 @@
+console.log("I'm just a js file.");
+
+
+var a = { 
+	one: 1, 
+	two: 2, 
+	three: 3, 
+	four: 4, 
+	five: 5 
+};
+Object
+_proto_: Object
+
+for (var i = 0, len = a.length; i < len; i++ ){
+  console.log(i, a[i]);
+}
